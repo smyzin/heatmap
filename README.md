ebc9715624b3c7fa2290a12d9b7ea7b34e42e48b
smyzin <smyzin.work@gmail.com>
2016-08-19 16:59:16 +0300

    Updates and fixes

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
 # Heatmap page by session
-Practical task from LPgenerator (Denis Kabalkin)
 
 ***
 
 ### Main features
 
- - Used clean JS (withou JS-frameworks)
+ - Used clean JS (only jQuery lib)
  - Show clicks by session
- - Show maximum clicks in current/choosed session
+ - Show maximum clicks in choosed session
  - Heat map (warm colors for most clickable places, cold for rare)
- - As DB used AppBase
+ - As DB used AppBase.io
 
 #### Used technologys
 
--- assets/js/connection.js
+++ assets/js/connection.js
@@ -5,47 +5,6 @@ var appbaseCon = new Appbase({
 	password: '2965f1bd-bc14-4e5b-a3c9-714351f9c1f1'
 });
 
-// function getData(num){
-// 	var id = ip + '-' + num;
-// 	var res;
-// 	appbaseCon.get({
-// 		type: "coordinate",
-// 		id: id
-// 	}).on('data', function(response) {
-// 		res = response._source.data;
-// 		console.log(res);
-// 		return res;
-// 	}).on('error', function(error) {
-// 		console.log(error);
-// 	});
-// }
-
-function getData(num) {
-	var id = ip + '-' + num;
-
-	appbaseCon.search({
-		type: "coordinate",
-		body: {
-			query: {
-				match_all: {}
-			}
-		}
-	}).on('data', function(res) {
-		for(var i = 0; i < res.hits.hits.length; i++){
-
-			if(res.hits.hits[i]['_id'] == id){
-				var resData = JSON.stringify(res.hits.hits[i]._source.data); //.data;
-				localStorage['coordinateBySession ' + num] = resData;
-				var clicks = JSON.stringify(res.hits.hits[i]._source.clicks);
-				$.cookie('clicksFromDB', clicks, {expires: 7, path: '/'})
-			}else{
-				console.log("No suchdata");
-			}
-		}
-	}).on('error', function(err) {
-		console.log("get() method failed with: ", err);
-	})
-}
 
 function writeData(max, heatData, num){
 	var jsonObject = {
@@ -66,23 +25,21 @@ function writeData(max, heatData, num){
 
 getRecord = (num) =>
 {
-  appbaseRef.search({
-    type: 'contact',
-    body: {
-      query: {
-        "match" :
-          {
-            id : num
-          }
-      }
-    }
-  }).on('data', function(res) {
-  	var resData = JSON.stringify(res.hits.hits[0]._source.data);
-  	localStorage['coordinateBySession ' + num] = resData;
-  	var clicks = JSON.stringify(res.hits.hits[0]._source.clicks);
-  	$.cookie('clicksFromDB', clicks, {expires: 7, path: '/'})
-  }).on('error', function(err) {
-    console.log("search error: ", err);
-  })
+	appbaseCon.get({
+		type: 'coordinate',
+		id : ip + '-' + num
+	}).on('data', function(res) {
+		if (res.found) {
+			var resData = JSON.stringify(res._source.data);
+			localStorage['coordinateBySession ' + num] = resData;
+			var clicks = JSON.stringify(res._source.clicks);
+			$.cookie('clicksFromDB', clicks, { expires: 7, path: '' });
+		}else{
+			console.log('Error');
+		};
+	}).on('error', function(err) {
+		console.log("search error: ", err);
+	})
 }
+
 
